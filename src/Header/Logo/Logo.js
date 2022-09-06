c8181925877cb2e4b21f83a8c7766470a0a7e020
brocahontaz <johan@rooter.se>
2022-09-06 21:53:49 +0200

    skeleton home

---
+++ src/Header/Logo/Logo.js
@@ -0,0 +1,12 @@
+import './Logo.css';
+
+function Logo() {
+  return (
+    <div className="Logo">
+      <span className="mainText">Johan & Catarina</span>
+      <span className="infoText">Vallkärra Kyrka 19 November 2022</span>
+    </div>
+  );
+}
+
+export default Logo;
---
+++ src/Main/Main.js
@@ -0,0 +1,7 @@
+import './Main.css';
+
+function Main() {
+  return <div className="Main" />;
+}
+
+export default Main;
