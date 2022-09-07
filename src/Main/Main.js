f79fb9feca1bc74ffcebf109d0ae7673710eeba0
brocahontaz <johan@rooter.se>
2022-09-07 23:02:18 +0200

    Added logo, navbar, scroll, skeleton for the rest

--- src/Main/Main.js
+++ src/Main/Main.js
@@ -1,7 +1,15 @@
 import './Main.css';
 
+import Home from './Home';
+import Story from './Story';
+
 function Main() {
-  return <div className="Main" />;
+  return (
+    <div className="Main">
+      <Home />
+      <Story />
+    </div>
+  );
 }
 
 export default Main;
