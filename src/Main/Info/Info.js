a7797a189a57cff4f4ab4ba05e739fd7e6151fd3
Catarina Sörensen <catarina.sorensen@jayway.com>
2022-09-10 21:48:36 +0200

    Schedule

---
+++ src/Main/Info/Info.js
@@ -0,0 +1,47 @@
+/*eslint-disable */
+import './Info.css';
+import Event from '../components/Event'
+
+function Info() {
+  return (
+    <div className="Info" id="info">
+      <h1>Observera</h1>
+      <div>Vi vill kunna bjuda många från familj och vänner så därför önskar vi en <b>kuvertavgift istället för present på vårt bröllop</b>. Det kommer vara 500kr per kuvert och det kommer (förhoppningsvis) bli fantastisk mat under middagen och stor variation av dryck till det, både med och utan alkohol. Ingen återbetalning av kuvertavgiften kan ske vid exempelvis sjukdom/förhinder då allting (och mer därtill) går oavkortat till cateringbolaget, försök håll er friska och krya så ni inte missar denna magiska dag.</div>
+      <h1>Preliminär tidsplan</h1>
+      <Event
+        thing="Vigsel"
+        location="Vallkärra kyrka"
+        extra="därefter promenad eller biltur till Axelgård"
+        time="14.00"
+      />
+      <Event
+        thing="Mingel, dryck & snacks - Axelgård lilla salen"
+        location="Axelgård - Stora salen"
+        extra=" "
+        time="efter vigsel"
+      />
+      <Event
+        thing="Middag"
+        location="Axelgård - Stora salen"
+        extra="3 rätters middag med dryck och underhållning"
+        time="17.30"
+      />
+      <Event
+        thing="Dans"
+        location="Axelgård - Stora salen"
+        extra="Inledningsvis slow dance och därefter untz "
+        time="efter middagen"
+      />
+
+      <Event
+        thing="Nattamat"
+        location="Axelgård"
+        extra="Enklare korv med bröd"
+        time="senast kl 01"
+      />
+    
+    </div>
+  );
+}
+
+export default Info;
--- src/Main/Main.js
+++ src/Main/Main.js
@@ -2,11 +2,13 @@ import './Main.css';
 
 import Home from './Home';
 import Story from './Story';
+import Info from './Info';
 
 function Main() {
   return (
     <div className="Main">
       <Home />
+      <Info />
       <Story />
     </div>
   );
