0d9d3c291f56b7b21e116f7c21024eb00d539a01
brocahontaz <johan@rooter.se>
2022-09-10 21:17:33 +0200

    who doesnt like a nice burger menu

--- src/Header/NavigationBurger/NavigationBurger.js
+++ src/Header/NavigationBurger/NavigationBurger.js
@@ -1,18 +1,41 @@
 import PropTypes from 'prop-types';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
-import { faBars } from '@fortawesome/free-solid-svg-icons';
+import { faBars, faXmark } from '@fortawesome/free-solid-svg-icons';
+import { useState } from 'react';
+import { NavHashLink } from 'react-router-hash-link';
 
 import './NavigationBurger.css';
 
 function NavigationBurger({ links }) {
+  const [navbarOpen, setNavbarOpen] = useState(false);
+
+  const handleToggle = () => {
+    setNavbarOpen((prev) => !prev);
+  };
+
+  const closeMenu = () => {
+    setNavbarOpen(false);
+  };
+
   return (
     <div className="NavigationBurger">
-      <button type="button">
-        <FontAwesomeIcon icon={faBars} size="2x" />
+      <button type="button" onClick={handleToggle}>
+        { !navbarOpen ? <FontAwesomeIcon icon={faBars} size="2x" /> : <FontAwesomeIcon icon={faXmark} size="2x" /> }
       </button>
 
-      <div className="Expandable">
-        {links}
+      <div className={`Navbar ${navbarOpen ? 'Open' : ''}`}>
+        {links.map((link) => (
+          <NavHashLink
+            key={`nav-${link}`}
+            smooth
+            to={`#${link}`}
+            className={({ isActive }) => `nav-link${isActive ? ' selected' : ''}`}
+            activeClassName="selected"
+            onClick={() => closeMenu()}
+          >
+            {link.toUpperCase()}
+          </NavHashLink>
+        ))}
       </div>
     </div>
   );
