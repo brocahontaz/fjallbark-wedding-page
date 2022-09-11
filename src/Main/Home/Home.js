f153c9cd742bddd9ca06c1ef10984339686f4469
brocahontaz <johan@rooter.se>
2022-09-11 20:13:57 +0200

    add rings to home + smol fixes

--- src/Main/Home/Home.js
+++ src/Main/Home/Home.js
@@ -1,10 +1,14 @@
 import './Home.css';
+import Rings from '../../assets/img/ringar-mork-square.png';
 
 function Home() {
   return (
     <div className="Home">
       <div className="Positioner">
         <div className="CoverChecker" id="hem" data-scrollspy />
+      </div>
+      <div className="HomeWrap">
+        <img src={Rings} alt="Rings" />
       </div>
     </div>
   );
