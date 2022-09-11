13f06f4999a1bcbc27b894dd7c1ad1fdbab69037
Johan Andersson <johan@rooter.se>
2022-09-11 10:18:07 +0200

    fix: content blocks (#15)

--- src/Main/Crew/Crew.js
+++ src/Main/Crew/Crew.js
@@ -1,24 +1,27 @@
 import './Crew.css';
+import ContentBlock from '../components/ContentBlock';
 
 function Crew() {
   return (
     <div id="crew" className="Crew" data-scrollspy>
-      <h2>Tärnor</h2>
-      <span>Jennifer Holst</span>
-      <span>Lisa Jakobsson</span>
-      <span>Sanna Holmgren</span>
-      <h2>Marshalker</h2>
-      <span>Ulrik</span>
-      <span>Daniel</span>
-      <span>Markus Maga</span>
-      <h2>Toastmaster och Toastmadame</h2>
-      <span>Elin Hanstorp</span>
-      <span>Felix</span>
-      <h2>Värdpar</h2>
-      <span>Kristoffer Linderman</span>
-      <span>Linnea</span>
-      <h2>DJ</h2>
-      <span>Nicholas Salas</span>
+      <ContentBlock>
+        <h2>Tärnor</h2>
+        <span>Jennifer Holst</span>
+        <span>Lisa Jakobsson</span>
+        <span>Sanna Holmgren</span>
+        <h2>Marshalker</h2>
+        <span>Ulrik</span>
+        <span>Daniel</span>
+        <span>Markus Maga</span>
+        <h2>Toastmaster och Toastmadame</h2>
+        <span>Elin Hanstorp</span>
+        <span>Felix</span>
+        <h2>Värdpar</h2>
+        <span>Kristoffer Linderman</span>
+        <span>Linnea</span>
+        <h2>DJ</h2>
+        <span>Nicholas Salas</span>
+      </ContentBlock>
     </div>
   );
 }
--- src/Main/Menu/Menu.js
+++ src/Main/Menu/Menu.js
@@ -1,42 +1,45 @@
 /* eslint-disable max-len */
+import ContentBlock from '../components/ContentBlock';
 import './Menu.css';
 
 function Menu() {
   return (
+
     <div className="Menu" id="mat" data-scrollspy>
+      <ContentBlock>
+        <h2>Preliminär meny</h2>
+        <br />
 
-      <h2>Preliminär meny</h2>
-      <br />
+        <span>
+          <b>Förätt</b>
+          {' '}
+          Confiterad torsk - havskräftsbisque - rostad butternutpumpa - citronemulsion - rökt sidfläsk - blommor
+        </span>
+        <br />
+        <br />
 
-      <span>
-        <b>Förätt</b>
-        {' '}
-        Confiterad torsk - havskräftsbisque - rostad butternutpumpa - citronemulsion - rökt sidfläsk - blommor
-      </span>
-      <br />
-      <br />
+        <span>
+          <b>Huvudrätt</b>
+          {' '}
+          Bräserad oxkind - sotad silverlök - bakad palsternacka - syltad pumpa - gremolata smaksatt potatispuré
+        </span>
+        <br />
+        <br />
 
-      <span>
-        <b>Huvudrätt</b>
-        {' '}
-        Bräserad oxkind - sotad silverlök - bakad palsternacka - syltad pumpa - gremolata smaksatt potatispuré
-      </span>
-      <br />
-      <br />
+        <span>
+          <b>Efterätt</b>
+          {' '}
+          Glenns Schwarzwaldtårta samt prinsesstårta!
+        </span>
 
-      <span>
-        <b>Efterätt</b>
-        {' '}
-        Glenns Schwarzwaldtårta samt prinsesstårta!
-      </span>
+        <br />
+        <br />
+        <br />
 
-      <br />
-      <br />
-      <br />
-
-      Maten kommer lagas och serveras av Segers Mat, givetvis tillgodoser de anmälda matpreferenser!
-
+        Maten kommer lagas och serveras av Segers Mat, givetvis tillgodoser de anmälda matpreferenser!
+      </ContentBlock>
     </div>
+
   );
 }
 
