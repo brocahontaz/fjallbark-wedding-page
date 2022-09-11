f0e74b7569034c405a964590408b7aa589af38b6
Johan Andersson <johan@rooter.se>
2022-09-11 10:33:42 +0200

    Added more sections, empty for now (#16)

---
+++ src/Main/Ceremony/Ceremony.js
@@ -0,0 +1,13 @@
+/* eslint-disable max-len */
+import './Ceremony.css';
+import ContentBlock from '../components/ContentBlock';
+
+function Ceremony() {
+  return (
+    <div className="Ceremony" id="vigsel" data-scrollspy>
+      <ContentBlock />
+    </div>
+  );
+}
+
+export default Ceremony;
--- src/Main/Crew/Crew.js
+++ src/Main/Crew/Crew.js
@@ -5,22 +5,24 @@ function Crew() {
   return (
     <div id="crew" className="Crew" data-scrollspy>
       <ContentBlock>
-        <h2>Tärnor</h2>
-        <span>Jennifer Holst</span>
-        <span>Lisa Jakobsson</span>
-        <span>Sanna Holmgren</span>
-        <h2>Marshalker</h2>
-        <span>Ulrik</span>
-        <span>Daniel</span>
-        <span>Markus Maga</span>
-        <h2>Toastmaster och Toastmadame</h2>
-        <span>Elin Hanstorp</span>
-        <span>Felix</span>
-        <h2>Värdpar</h2>
-        <span>Kristoffer Linderman</span>
-        <span>Linnea</span>
-        <h2>DJ</h2>
-        <span>Nicholas Salas</span>
+        <div className="CrewWrap">
+          <h2>Tärnor</h2>
+          <span>Jennifer Holst</span>
+          <span>Lisa Jakobsson</span>
+          <span>Sanna Holmgren</span>
+          <h2>Marshalker</h2>
+          <span>Ulrik Sjölin</span>
+          <span>Daniel Ohlsson</span>
+          <span>Markus Maga</span>
+          <h2>Toastmaster och Toastmadame</h2>
+          <span>Elin Hanstorp</span>
+          <span>Felix Rosenlund</span>
+          <h2>Värdpar</h2>
+          <span>Kristoffer Linderman</span>
+          <span>Linnea</span>
+          <h2>DJ</h2>
+          <span>Nicholas Salas</span>
+        </div>
       </ContentBlock>
     </div>
   );
