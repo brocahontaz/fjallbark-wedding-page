98d736261a6ae2fe9a3b35afecbfaa4ede0b81fc
Johan Andersson <johan@rooter.se>
2022-09-14 19:52:46 +0200

    Feat: more info and layout (#28)
    * looots of stuff
    
    * even more info

--- src/Main/Ceremony/Ceremony.js
+++ src/Main/Ceremony/Ceremony.js
@@ -9,7 +9,58 @@ function Ceremony() {
       <div className="Positioner">
         <div className="CoverChecker" id="vigsel" data-scrollspy />
       </div>
-      <ContentBlock title="Vigsel" subtitle="Vallkärra Kyrka" image={Church} />
+      <ContentBlock title="Vigsel" subtitle="Vallkärra Kyrka" image={Church}>
+        <p>
+          Själva ceremonin kommer äga rum i den vackra
+          {' '}
+          <b>Vallkärra Kyrka</b>
+          {' '}
+          strax norr om Lund, med anor från slutet av 1100-talet.
+        </p>
+        <h3>Hur tar jag mig hit?</h3>
+        <p>
+          Allra enklast är med
+          {' '}
+          <b>bil</b>
+          , och det finns gott om parkeringsmöjligheter alldels vid kyrkan.
+        </p>
+        <p>
+          Om jag åker kollektivt så stannar både
+          {' '}
+          <b>Stadsbuss 21</b>
+          {' '}
+          i närheten, vid hållplats
+          {' '}
+          <i>Lund Vallkärra by</i>
+          , samt
+          {' '}
+          <b>Regionbuss 123</b>
+          {' '}
+          vid hållplats
+          {' '}
+          <i>Vallkärra k:a</i>
+          .
+        </p>
+        <p>
+          <b>Adress:</b>
+          {' '}
+          Kävlingevägen 107, 226 50 Lund
+        </p>
+
+        <iframe
+          title="churchmap"
+          className="MapFrame"
+          width="100%"
+          height="350"
+          frameBorder="0"
+          scrolling="no"
+          marginHeight="0"
+          marginWidth="0"
+          src="https://www.openstreetmap.org/export/embed.html?bbox=13.155698776245119%2C55.73580928511603%2C13.187413215637209%2C55.74616122293971&amp;layer=mapnik&amp;marker=55.740979557644664%2C13.171555995941162"
+        />
+        <br />
+        <small><a href="https://www.openstreetmap.org/?mlat=55.7410&amp;mlon=13.1716#map=16/55.7410/13.1716&amp;layers=N">View Larger Map</a></small>
+      </ContentBlock>
     </div>
   );
 }
--- src/Main/Party/Party.js
+++ src/Main/Party/Party.js
@@ -9,7 +9,67 @@ function Party() {
       <div className="Positioner">
         <div className="CoverChecker" id="fest" data-scrollspy />
       </div>
-      <ContentBlock title="Fest" subtitle="Axelgård" image={Bal} />
+      <ContentBlock title="Fest" subtitle="Axelgård" image={Bal}>
+        <p>
+          Efter vigseln beger vi oss mot
+          {' '}
+          <b>Axelgård</b>
+          {' '}
+          för mingel, mat, och festligheter. Gården är mysig en gammal fyrlängad bondgård, där vi har tillgång till det gamla boningshuset i två längor.
+        </p>
+        <p>
+          Middagen och festen äger rum i Stora Salen, medans vi kommer ha tillgång till lite mingel och annat kul i Lilla Salen.
+        </p>
+        <p>
+          <b>Viktigt att tänka på</b>
+          {' '}
+          är att det huserar ett stall fullt med hästar i de övriga två längorna. Det är
+          {' '}
+          <i>inte</i>
+          {' '}
+          tillåtet för någon gäst att befinna sig i stallbyggnaden under några omständigheter, även om brudens alldeles egna löpare bor där.
+        </p>
+        <h3>
+          Underhållning
+        </h3>
+        <h4 className="center-text">DJ Nicolas Salas</h4>
+        <p className="center-text">
+          ..kommer stå bakom spakarna under festen och bjuda på goa bitar!
+        </p>
+        <h4 className="center-text">
+          Selfie Cam
+        </h4>
+        <p className="center-text">
+          Det kommer att finnas en selfie-cam-station i någon hörna av lokalen, där alla gäster är mer än välkomna att föreviga roliga ögonblick!
+        </p>
+        <h3>
+          Hur tar jag mig hit?
+        </h3>
+        <p>
+          Axelgård befinner sig på ett mycket kort promenadavstånd från kyrkan. För de som åker kollektivt, eller helt sonika känner för att ta en promenad, så går detta alldeles utmärkt.
+        </p>
+        <p>
+          För de med bil som även vill flytta denna till festlokalen, så finns det gott om parkeringsplatser. Tänk dock på att inte parkera på innergården.
+        </p>
+        <p>
+          <b>Adress:</b>
+          {' '}
+          Västratornsvägen 1, 226 50 Lund
+        </p>
+        <iframe
+          title="partymap"
+          className="MapFrame"
+          width="100%"
+          height="350"
+          frameBorder="0"
+          scrolling="no"
+          marginHeight="0"
+          marginWidth="0"
+          src="https://www.openstreetmap.org/export/embed.html?bbox=13.17138433456421%2C55.735736799791724%2C13.183486461639406%2C55.74091312157089&amp;layer=mapnik&amp;marker=55.73832504649804%2C13.177435398101807"
+        />
+        <br />
+        <small><a href="https://www.openstreetmap.org/?mlat=55.73833&amp;mlon=13.17744#map=17/55.73833/13.17744&amp;layers=N">View Larger Map</a></small>
+      </ContentBlock>
     </div>
   );
 }
