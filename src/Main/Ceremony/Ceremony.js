d8b8267a3f1a0024eb341f6fb9691fc63dced3c4
brocahontaz <johan@rooter.se>
2022-09-14 19:34:04 +0200

    looots of stuff

--- src/Main/Ceremony/Ceremony.js
+++ src/Main/Ceremony/Ceremony.js
@@ -9,7 +9,33 @@ function Ceremony() {
       <div className="Positioner">
         <div className="CoverChecker" id="vigsel" data-scrollspy />
       </div>
-      <ContentBlock title="Vigsel" subtitle="Vallkärra Kyrka" image={Church} />
+      <ContentBlock title="Vigsel" subtitle="Vallkärra Kyrka" image={Church}>
+        <p>
+          Själva ceremonin kommer äga rum i den vackra
+          {' '}
+          <b>Vallkärra Kyrka</b>
+          {' '}
+          strax norr om Lund, med anor från slutet av 1100-talet.
+        </p>
+        <p>
+          <b>Adress:</b>
+          {' '}
+          Kävlingevägen 107, 226 50 Lund
+        </p>
+        <iframe
+          title="churchmap"
+          className="MapFrame"
+          width="100%"
+          height="350"
+          frameBorder="0"
+          scrolling="no"
+          marginHeight="0"
+          marginWidth="0"
+          src="https://www.openstreetmap.org/export/embed.html?bbox=13.155698776245119%2C55.73580928511603%2C13.187413215637209%2C55.74616122293971&amp;layer=mapnik&amp;marker=55.740979557644664%2C13.171555995941162"
+        />
+        <br />
+        <small><a href="https://www.openstreetmap.org/?mlat=55.7410&amp;mlon=13.1716#map=16/55.7410/13.1716&amp;layers=N">View Larger Map</a></small>
+      </ContentBlock>
     </div>
   );
 }
--- src/Main/Info/Info.js
+++ src/Main/Info/Info.js
@@ -1,5 +1,7 @@
 /* eslint-disable max-len */
 import './Info.css';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { faEnvelope } from '@fortawesome/free-solid-svg-icons';
 import Event from '../components/Event';
 import ContentBlock from '../components/ContentBlock';
 import Ringbubbel from '../../assets/img/ringbubbel.png';
@@ -31,10 +33,10 @@ function Info() {
             Ingen återbetalning av kuvertavgiften kan ske vid exempelvis sjukdom/förhinder då allting (och mer därtill) går oavkortat till cateringbolaget, försök håll er friska och krya så ni inte missar denna magiska dag.
           </p>
           <h3>Klädkod</h3>
-          <p>
+          <div className="subtitle">
             Vad ska jag ha på mig på detta fantastiska kalas?
-          </p>
-          <h4>
+          </div>
+          <h4 className="center-text">
             Högtidsdräkt eller Mörk kostym
           </h4>
           <p>
@@ -135,42 +137,69 @@ function Info() {
               </ul>
             </li>
           </ul>
-          <h3>Preliminär tidsplan</h3>
+          <h3>Jag vill hålla tal!</h3>
           <div className="subtitle">
-            Lördag 19 November 2022
+            Vad roligt!
           </div>
-          <Event
-            thing="Vigsel"
-            location="Vallkärra kyrka"
-            extra="därefter promenad eller biltur till Axelgård"
-            time="14.00"
-          />
-          <Event
-            thing="Mingel, dryck & snacks"
-            location="Axelgård - Lilla salen"
-            extra=" "
-            time="efter vigsel"
-          />
-          <Event
-            thing="Middag"
-            location="Axelgård - Stora salen"
-            extra="3 rätters middag med dryck och underhållning"
-            time="17.30"
-          />
-          <Event
-            thing="Dans"
-            location="Axelgård - Stora salen"
-            extra="Inledningsvis slow dance och därefter untz "
-            time="efter middagen"
-          />
-
-          <Event
-            thing="Nattamat"
-            location="Axelgård"
-            extra="Enklare korv med bröd"
-            time="senast kl 01"
-          />
+          <p>
+            Om Ni vill hålla tal, slå en kullerbytta, eller hitta på något annat bus under middagen/festen så uppmuntras detta naturligtvis!
+          </p>
+          <p>
+            Behöver Ni stämma av något med brudparet, så går det bra att kontakta oss direkt på
+            {' '}
+            <a href="mailto:bröllop@fjällbark.se">
+              <FontAwesomeIcon icon={faEnvelope} size="1x" />
+              {' '}
+              bröllop@fjällbark.se
+            </a>
+          </p>
+          <p>
+            Om det är något Ni istället vill hålla lite hemligt för brudparet? Ja men då går det ypperligt att istället kontakta våra eminenta toastmasters på
+            {' '}
+            <a href="mailto:toastmaster@fjällbark.se">
+              <FontAwesomeIcon icon={faEnvelope} size="1x" />
+              {' '}
+              toastmaster@fjällbark.se
+            </a>
+          </p>
         </div>
+      </ContentBlock>
+      <ContentBlock>
+        <h3>Preliminär tidsplan</h3>
+        <div className="subtitle">
+          Lördag 19 November 2022
+        </div>
+        <Event
+          thing="Vigsel"
+          location="Vallkärra kyrka"
+          extra="därefter promenad eller biltur till Axelgård"
+          time="14.00"
+        />
+        <Event
+          thing="Mingel, dryck & snacks"
+          location="Axelgård - Lilla salen"
+          extra=" "
+          time="efter vigsel"
+        />
+        <Event
+          thing="Middag"
+          location="Axelgård - Stora salen"
+          extra="3 rätters middag med dryck och underhållning"
+          time="17.30"
+        />
+        <Event
+          thing="Dans"
+          location="Axelgård - Stora salen"
+          extra="Inledningsvis slow dance och därefter untz "
+          time="efter middagen"
+        />
+
+        <Event
+          thing="Nattamat"
+          location="Axelgård"
+          extra="Enklare korv med bröd"
+          time="senast kl 01"
+        />
       </ContentBlock>
     </div>
   );
