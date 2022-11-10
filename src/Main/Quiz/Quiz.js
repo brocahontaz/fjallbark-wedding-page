9a3ab3077293b478e66c25c361a0cfa257b3f1f8
brocahontaz <johan@rooter.se>
2022-11-10 22:19:37 +0100

    layout fixes

--- src/Main/Quiz/Quiz.js
+++ src/Main/Quiz/Quiz.js
@@ -7,8 +7,8 @@ function Quiz() {
         <iframe
           title="quiz"
           src="https://docs.google.com/forms/d/e/1FAIpQLSeCUBm-02XZ_FBmZIUp_zFvHvZaRnp0KMJQ5eVVIc3x7KZ1cg/viewform?embedded=true"
-          width="640"
-          height="800"
+          width="100%"
+          height="800px"
           frameBorder="0"
           marginHeight="0"
           marginWidth="0"
