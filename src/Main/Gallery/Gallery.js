7c73edfd54f8a38ca53e5e5ccc87aa217a6c1464
brocahontaz <johan@rooter.se>
2023-11-05 20:05:05 +0100

    lightbox non working

--- src/Main/Gallery/Gallery.js
+++ src/Main/Gallery/Gallery.js
@@ -1,4 +1,14 @@
+import { useState } from "react";
+
 import PhotoAlbum from "react-photo-album";
+import Lightbox from "yet-another-react-lightbox";
+// import "yet-another-react-lightbox/styles.css";
+// import "yet-another-react-lightbox/plugins/thumbnails.css";
+// import Fullscreen from "yet-another-react-lightbox/plugins/fullscreen";
+// import Slideshow from "yet-another-react-lightbox/plugins/slideshow";
+// import Thumbnails from "yet-another-react-lightbox/plugins/thumbnails";
+// import Zoom from "yet-another-react-lightbox/plugins/zoom";
+
 import "./Gallery.css";
 
 function getUniqueListBy(arr, key) {
@@ -16,11 +26,14 @@ const photos = filenames.map((ref) => ({ src: ref, width: 200, height: 200 }));
 const photosArr = getUniqueListBy(photos, "src");
 
 function Gallery() {
+  const [index, setIndex] = useState(-1);
+
   return (
     <div className="Gallery">
       <PhotoAlbum
         layout="masonry"
         photos={photosArr}
+        onClick={({ clickedIndex }) => setIndex(clickedIndex)}
         renderPhoto={({ photo, wrapperStyle, renderDefaultPhoto }) => (
           <a
             href={photo.href}
@@ -31,6 +44,15 @@ function Gallery() {
             {renderDefaultPhoto({ wrapped: true })}
           </a>
         )}
+      />
+
+      <Lightbox
+        slides={photos}
+        open={index >= 0}
+        index={index}
+        close={() => setIndex(-1)}
+        // enable optional lightbox plugins
+        // plugins={[Fullscreen, Slideshow, Thumbnails, Zoom]}
       />
     </div>
   );
